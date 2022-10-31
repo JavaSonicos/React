b0d8683d054ffe37bfb8268522db43161cdb250a
fabiandres14 <fabianbm147@gmail.com>
2022-10-30 22:04:19 -0500

    Agregando, pero tiene errores para correccion_Fabian Borrero

--- src/components/paginas/Carrito.js
+++ src/components/paginas/Carrito.js
@@ -1,12 +1,74 @@
-import React from 'react'
+import { Component  } from "react";
+import '../Estilos/Tabla_Carrito.css';
 
-export function Carrito(){
-    return (
-      <div >
-        <h1>Hola Carrito</h1>
- 
-      </div>
-    );
+
+
+
+
+var heading = ['Id Producto', 'Nombre Producto', 'Descripcion Producto', 'Valor Unitario'];
+
+
+
+var body =
+    [
+        ['0001', 'Producto 1', 'Unidades dependiendo Producto','10']
+
+    ];
+
+
+export function Carrito() {
+
+  return (
+      <>
+          <div id='Tabla_Carrito'>
+              <h3>Lista de Compras</h3>
+              <br></br>
+              <div >
+                  <Table/>
+              </div>
+              <h4>Total de la Venta</h4>
+              <button>Confirme su Compra</button>
+          </div>
+      </>
+  );
+
+}
+
+class Table extends Component {
+  render() {
+
+      
+      
+
+      var heading = this.props.heading;
+      var body = this.props.body;
+
+      return (
+          <div id="datagrid">
+              <table>
+                  <thead>
+                      <tr>
+                          {heading.map(head => <th>{head}</th>)}
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {body.map(row => <TableRow row={row} />)}
+                  </tbody>
+              </table>
+
+
+          </div>
+      );
   }
-  
-  
+}
+
+class TableRow extends Component {
+  render() {
+      var row = this.props.row;
+      return (
+          <tr>
+              {row.map(val => <td>{val}</td>)}
+          </tr>
+      )
+  }
+}
